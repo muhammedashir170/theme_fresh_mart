2820449786d39cc8d66e99abbc08f5988f758767
MUHAMMED ASHIR <muhammedashir170@gmail.com>
2025-11-21 13:46:33 +0530

    added live link

--- theme_fresh_mart/__manifest__.py
+++ theme_fresh_mart/__manifest__.py
@@ -22,6 +22,7 @@
     """,
     'license': 'OPL-1',
     'support': 'teamxapp.dev@gmail.com',
+    'live_test_url': 'http://13.60.52.65:8071',
     'price': 169.00,
     'currency': 'USD',
     'depends': [
